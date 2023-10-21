26bceae2352fe6eeb5782affbee24ad61f7c2ab1
soumia <soumia.sahtani@gmail.com>
2023-10-21 22:08:22 +0100

    add modification

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-// let menu = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
+// BUREDER MENU
+let menu = document.getElementById("nav");
+let list=document.getElementById("list");
+console.log(list);
+list.onclick=function(){
+    if(menu.style.display === "block") {
+        menu.style.display="none";
+    }else {
+        menu.style.display="block";
+    }
+}
 
-// menu.onclick = () => {
-//     menu.classList.toggle('bx-x');
-//     navbar.classList.toggle('open');
-// };
-let menuBurger = document.querySelector(".menu-burger");
-let closeMenu = document.querySelector(".close-menu");
-let navMenu = document.querySelector(".navigation");
-const openNavMenu = ()=>{
-    navMenu.style.transform = 'translateX(0)';
-};
-const closeNavMenu = ()=>{
-    navMenu.style.transform = 'translateX(100%)';
-};
-menuBurger.addEventListener('click', openNavMenu);
-closeMenu.addEventListener('click', closeNavMenu);
+
+// FAQ ANSWER
+
+const faqs = document.querySelectorAll('.check'); 
+faqs.forEach(check => {
+   const answer=check.querySelector('.answer');
+   check.addEventListener('click',()=>{
+        answer.classList.toggle("active")
+
+    });
+
+})
+
+
+
+//REJEX
+const form = document.getElementById("myform");
+        const result = document.getElementById("result");
+
+        form.addEventListener("submit", function (event) {
+            event.preventDefault(); // Empêche l'envoi par défaut du formulaire
+
+            if (form.checkValidity()) {
+                result.innerHTML = "Le formulaire est valide.";
+                result.style.display="block";
+
+            } else {
+                result.innerHTML = "Le formulaire contient des erreurs. Veuillez vérifier les champs.";
+                result.style.display="block";
+            }
+        });
