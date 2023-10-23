9369cb6c484089c6d47666ba12197260d0a12876
soumia <soumia.sahtani@gmail.com>
2023-10-23 20:35:52 +0100

    final

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-// BUREDER MENU
-// let menu = document.getElementById("nav");
-// let list=document.getElementById("list");
-// console.log(list);
-// list.onclick=function(){
-//     if(menu.style.display === "block") {
-//         menu.style.display="none";
-//     }else {
-//         menu.style.display="block";
-//     }
-// }
+// BURGER MENU
 const hamburger = document.querySelector("#list");
 const navMenu = document.querySelector(".nav-links");
 
@@ -21,8 +11,8 @@ hamburger.addEventListener("click", ()=>{
 document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
 
+}))
 
 // FAQ ANSWER
 
@@ -36,21 +26,42 @@ faqs.forEach(check => {
 
 })
 
+// REJEX
+document.getElementById('myform').addEventListener('submit', function (e) {
+  e.preventDefault();
+      const fullname = document.getElementById("name").value; 
+      const email = document.getElementById("email").value;
+      const cin = document.getElementById("cin").value;
+      const telephone = document.getElementById("phone").value;
+      const result = document.getElementById("result");
+      
+      const nameRegex = /^[A-Za-zÀ-ÖØ-öø-ÿ]+\s[A-Za-zÀ-ÖØ-öø-ÿ]+$/;
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      const cinRegex = /^HH[0-9]{5}$/;
+      const numberRegex = /^\+212[0-9]{9}$/;
 
+      if (!fullname.match(nameRegex)) {
+        alert("Name Invalid.");
+        return false;
+      }
+      if (!email.match(emailRegex)) {
+        alert("Email Invalid.");
+        return false;
+      }
+      if (!cin.match(cinRegex)) {
+        alert("CIN invlaid.");
+        return false;
+      }
 
-//REJEX
-const form = document.getElementById("myform");
-        const result = document.getElementById("result");
-
-        form.addEventListener("submit", function (event) {
-            event.preventDefault(); // Empêche l'envoi par défaut du formulaire
-
-            if (form.checkValidity()) {
-                result.innerHTML = "Le formulaire est valide.";
+      if (!telephone.match(numberRegex)) {
+        alert("Le numéro de téléphone doit commencer par +212 suivi de 9 chiffres.");
+        return false;
+      }
+      result.innerHTML = "Le formulaire est valide.";
                 result.style.display="block";
 
-            } else {
-                result.innerHTML = "Le formulaire contient des erreurs. Veuillez vérifier les champs.";
-                result.style.display="block";
-            }
-        });
+    }
+  
+
+)
+
